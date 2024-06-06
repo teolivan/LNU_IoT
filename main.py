3473948988e22a08d8976a973cc3b89f02a78c43
teolivan <74550333+teolivan@users.noreply.github.com>
2024-06-06 15:20:25 +0200

    add temp

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-# Import from libraries
+import machine
 import time
-from machine import Pin
 
-# Set the OUTPUT pin to on-board LED
-led = Pin("LED", Pin.OUT)
+adc = machine.ADC(27)
+sf = 4095/65535 # Scale factor
+volt_per_adc = (3.3 / 4095)
 
-# Runs forever
-while True:
-    led.on()              # Turn on LED
-    time.sleep(0.2)       # Delay for 0.2 seconds
-    led.off()             # Turn off LED
-    time.sleep(1.0)       # Delay for 1.0 seconds
+while True: 
+    time.sleep(2)
+    millivolts = adc.read_u16()
+
+    adc_12b = millivolts * sf
+
+    volt = adc_12b * volt_per_adc
+
+    # MCP9700 characteristics
+    dx = abs(50 - 0)
+    dy = abs(0 - 0.5)
+
+    shift = volt - 0.5
+
+    temp = shift / (dy / dx)
+    print(temp)
+    time.sleep(1)
